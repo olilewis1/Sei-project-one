22e5cc485d206ac8e12c09b7a001c7131f928ed5
olilewis1 <olilewis1@hotmail.co.uk>
2021-02-20 11:49:11 +0000

    added snake to page and moving as just one square

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,50 @@ function init () {
   const cellCount = width * width 
   const cells = []
 
+  //Snake// 
+  const snakeClass = 'snake'
+  const snakeStartPosition = 0 
+  let snakeCurrentPosition = 0 
+
+  function createGrid (snakeStartPosition) { 
+    for (let i = 0; i < cellCount; i++) { 
+      const cell = document.createElement('div')
+      cell.textContent = i  
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+    addSnake(snakeStartPosition)
+  }
+
+  function addSnake(position) { 
+    cells[position].classList.add(snakeClass)
+  }
+
+  function removeSnake(position) { 
+    cells[position].classList.remove(snakeClass)
+  }
+
+  function handleKeyUp(event) { 
+    const key = event.keyCode
+    removeSnake(snakeCurrentPosition)
+    if (key === 39 && snakeCurrentPosition % width !== width - 1 ) { 
+      snakeCurrentPosition++ 
+    } else if (key === 37 && snakeCurrentPosition % width !== 0) { 
+      snakeCurrentPosition--
+    } else if (key === 38 && snakeCurrentPosition >= width) { 
+      snakeCurrentPosition -= width
+    } else if (key === 40 && snakeCurrentPosition + width <= width * width - 1) { 
+      snakeCurrentPosition += width
+    } else { 
+      console.log('invalid key')
+    }
+    addSnake(snakeCurrentPosition)
+  }
+
+  document.addEventListener('keydown', handleKeyUp)
+
+
+  createGrid(snakeStartPosition)
 
 
 
